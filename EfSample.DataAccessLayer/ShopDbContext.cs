034fa7fbc8215f1a34dca63625bf9083b8f9650a
Parham Darvishi <darvishiparham14@gmail.com>
2024-01-09 22:30:49 +0330

    Add project files.

---
+++ EfSample.DataAccessLayer/ShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EfSample.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EfSample.DataAccessLayer
+{
+    internal class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options):base()
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+    }
+}
---
+++ EfSample.Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace EfSample.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
